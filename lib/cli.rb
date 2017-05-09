8fbaba0108533b17501741e06e39eab6d551d4b0
Kaisa Piipari <kaisa.piipari@gmail.com>
2017-05-10 00:11:58 +0100

    Updates the run command

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,24 +4,41 @@ require 'nokogiri'
 
 class CommandLineInterface
 
-    URL = "./fixtures/index.html"
+    #URL = "./fixtures/index.html"
     #URL = "https://www.bbcgoodfood.com/recipes/collection/batch-cooking"
+    BASE_URL = "https://www.bbcgoodfood.com/recipes/collection/"
 
     def run
-        puts "Welcome to Grater! \n\n"
-        puts "Please select from the following recipes: \n\n"
-        create_recipe_list
+        input = user_commands
+        puts "#{user_commands}"
+
+        
+        #puts "Welcome to Grater! \n\n"
+        #puts "Please select from the following recipes: \n\n"
+        create_recipe_list(input)
         display_recipe_list
         display_full_recipe
     end
+
+    def user_commands
+        input = gets.chomp
+        meal_type = input.split(" ").last
+
+        if input.include?("grater") && meal_type != "grater"
+            meal_type
+        else
+            input = gets.chomp
+        end
+    end
+
 
     def user_interaction
         input = gets.chomp.to_i - 1
         input
     end
 
-    def create_recipe_list
-        recipe_list = Scraper.recipe_index_page_scraper(URL)
+    def create_recipe_list(page_url)
+        recipe_list = Scraper.recipe_index_page_scraper(BASE_URL + page_url)
         Grater.recipe_creator(recipe_list)
     end
 
